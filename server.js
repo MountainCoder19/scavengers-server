fadd4f06ea17e44c06b5c494d19ccaa033927ee2
jmrigali <jmrigali@ymail.com>
2017-08-29 12:08:33 -0600

    added ibm watson functionality need to set up post route to classify image from react app

--- server.js
+++ server.js
@@ -6,16 +6,17 @@ const bodyParser = require('body-parser');
 const knex = require('knex');
 const path = require('path');
 const cors = require('cors');
+const watson = require('./routes/watson.js');
 const user = require('./routes/user.js');
 app.use(cors());
 const hunts = require('./routes/hunts.js')
 app.use(bodyParser.urlencoded({ extended:false }));
 app.use(bodyParser.json());
 
-app.use(express.static(path.join('XXXXXX')));
+// app.use(express.static(path.join('XXXXXX')));
 app.use('/user', user);
 app.use('/hunts', hunts);
-// app.use('/messages',messages);
+// app.use('/classify', watson);
 
 const port = process.env.PORT || 3000;
 
