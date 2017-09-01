136ccac9ee411f371775eb8e2bdf037e0f9b8445
jmrigali <jmrigali@ymail.com>
2017-09-01 09:34:51 -0600

    added multer and took out fs.writefile functionality

--- routes/watson.js
+++ routes/watson.js
@@ -7,7 +7,7 @@ const ffmpeg = require('../image-resizer/ffmpeg.js');
 require('dotenv').load();
 const watson = require('watson-developer-cloud');
 const resizer = require('../image-resizer/resizer');
-
+const multer= require('multer');
 
 // router.get('/dontUSE', ()=>{
 //   // var key = require('./auth');
@@ -27,25 +27,39 @@ const resizer = require('../image-resizer/resizer');
 //
 //   return setTimout(response, 5000);
 // }
-router.post('/:endpoint', (req,res,next)=>{
+const storage = multer.diskStorage({
+  destination: './files',
+  filename(req, file, cb) {
+    cb(null, `${new Date()}-${file.originalname}`);
+  },
+});
+
+const upload = multer({ storage });
+
+router.post('/:endpoint', upload.single('file'), (req,res,next)=>{
+  const file = req.file; // file passed from client
+  const meta = req.body; // all other values passed from the client, like name, etc..
 
   let endpoint = req.params.endpoint;
-  const imgdata = req.body.data;
-  var imgpath = path.join(process.cwd(), '/temp/userimg.jpg')
-  // const imgpath = './temp/userimg.jpg'
-  const base64Data = imgdata.replace(/^data:([A-Za-z-+/]+);base64,/, '');
-  console.log('WRITEFILE', imgpath, typeof imgpath);
+  // const imgdata = req.body.data;
 
-  fs.writeFile('userimg.jpg', base64Data, 'utf8', (err, suc) => {
-    const filePathsToResize = [imgpath] // it can be just one, but still has to be an array, you can give it all of your images, it will just spit out images that are already below the configured max size (2MB) without doing any processing, so no need to check on your end
+  // var imgpath = path.join(process.cwd(), '/temp/userimg.jpg');
+
+  // const imgpath = './temp/userimg.jpg'
+  // const base64Data = imgdata.replace(/^data:([A-Za-z-+/]+);base64,/, '');
+
+  // fs.writeFile('userimg.jpg', imgdata, 'base64', (err, suc) => {
+    // const filePathsToResize = [imgpath] // it can be just one, but still has to be an array, you can give it all of your images, it will just spit out images that are already below the configured max size (2MB) without doing any processing, so no need to check on your end
     // const Resizer = new resizer.Resizer(filePathsToResize)
     // // use the Resizer.resize() method as a simple promise
     // Resizer.resize().then((filePaths) => {
     //  console.log('FILEPATHS POST RESIZER', filePaths) // paths to the resized images
      var params = {
-        images_file: fs.createReadStream(filePathsToResize),
+        images_file: file,
         'classifier_ids':[`${endpoint}`]
       }
+      console.log('PARAMS', params);
+      console.log("FILE", file);
       var visual_recognition = watson.visual_recognition({
         api_key: process.env.WATSON_API,
         version: 'v3',
@@ -64,12 +78,13 @@ router.post('/:endpoint', (req,res,next)=>{
           }
         })
         let result = JSON.stringify(resultTemp, null, 2)
+        console.log('RESULT', result);
         res.send(result)
       })
     // }).catch((error) => {
     //  console.log('error 2', error);
   //  })//END OF CATCH STATEMENT OF RESIZE
- })//END OF WRITE FILE
+ // })//END OF WRITE FILE
 })//END OF POST
 
 
