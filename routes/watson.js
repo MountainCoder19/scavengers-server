aa668aa1e7eee40084a697c2c389eb787313aa2a
jmrigali <jmrigali@ymail.com>
2017-09-01 12:13:16 -0600

    commented out watson functionality

--- routes/watson.js
+++ routes/watson.js
@@ -10,25 +10,6 @@ const resizer = require('../image-resizer/resizer');
 const multer = require('multer');
 
 
-// router.get('/dontUSE', ()=>{
-//   // var key = require('./auth');
-//
-//   // visual_recognition.listClassifiers({},
-//   //   function(err, response) {
-//   //     if (err)
-//   //     console.log(err);
-//   //     else
-//   //     console.log(JSON.stringify(response, null, 2));
-//   //   }
-//   // );
-//
-// })
-
-// function IBMClassify(filepath){
-//
-//   return setTimout(response, 5000);
-// }
-
 const storage = multer.diskStorage({
   destionation: './files',
   filename(req, file, cb){
@@ -38,10 +19,9 @@ const storage = multer.diskStorage({
 
 const upload = multer({storage});
 
-router.post('https://scavengers-server.herokuapp.com/:endpoint', upload.single('file'), (req,res,next)=>{
+router.post('/:endpoint', upload.single('file'), (req,res,next)=>{
   let file = req.file;
   let meta = req.body;
-  console.log(file);
   let endpoint = req.params.endpoint;
   console.log('PATH',  file.path);
   console.log('NAME', file.filepath);
@@ -52,30 +32,30 @@ router.post('https://scavengers-server.herokuapp.com/:endpoint', upload.single('
     // // use the Resizer.resize() method as a simple promise
     // Resizer.resize().then((filePaths) => {
     //  console.log('FILEPATHS POST RESIZER', filePaths) // paths to the resized images
-     var params = {
-        images_file: fs.createReadStream(file.path),
-        'classifier_ids':[`${endpoint}`]
-      }
-      var visual_recognition = watson.visual_recognition({
-        api_key: process.env.WATSON_API,
-        version: 'v3',
-        version_date: '2016-05-19',
-      })
-      visual_recognition.classify(params, function(err, response) {
-        if (err)
-        console.log('error', err);
-        else
-        console.log(JSON.stringify(response, null, 2))
-        var resultTemp= [];
-        let classesResponse = response.images[0].classifiers[0].classes;
-        classesResponse.forEach(el=>{
-          if(el.score > .60){
-            resultTemp.push(el)
-          }
-        })
-        let result = JSON.stringify(resultTemp, null, 2)
-        res.send(result)
-      })
+    //  var params = {
+    //     images_file: fs.createReadStream(file.path),
+    //     'classifier_ids':[`${endpoint}`]
+    //   }
+    //   var visual_recognition = watson.visual_recognition({
+    //     api_key: process.env.WATSON_API,
+    //     version: 'v3',
+    //     version_date: '2016-05-19',
+    //   })
+    //   visual_recognition.classify(params, function(err, response) {
+    //     if (err)
+    //     console.log('error', err);
+    //     else
+    //     console.log(JSON.stringify(response, null, 2))
+    //     var resultTemp= [];
+    //     let classesResponse = response.images[0].classifiers[0].classes;
+    //     classesResponse.forEach(el=>{
+    //       if(el.score > .60){
+    //         resultTemp.push(el)
+    //       }
+    //     })
+      //   let result = JSON.stringify(resultTemp, null, 2)
+      //   res.send(result)
+      // })
     // }).catch((error) => {
     //  console.log('error 2', error);
   //  })//END OF CATCH STATEMENT OF RESIZE
