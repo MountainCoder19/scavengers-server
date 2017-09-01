658b4c1efdd04377e685b5048bd9873960a648e9
jmrigali <jmrigali@ymail.com>
2017-09-01 14:52:13 -0600

    uncommented out watson stuff

--- routes/watson.js
+++ routes/watson.js
@@ -32,30 +32,30 @@ router.post('/:endpoint', upload.single('file'), (req,res,next)=>{
     // // use the Resizer.resize() method as a simple promise
     // Resizer.resize().then((filePaths) => {
     //  console.log('FILEPATHS POST RESIZER', filePaths) // paths to the resized images
-    //  var params = {
-    //     images_file: fs.createReadStream(file.path),
-    //     'classifier_ids':[`${endpoint}`]
-    //   }
-    //   var visual_recognition = watson.visual_recognition({
-    //     api_key: process.env.WATSON_API,
-    //     version: 'v3',
-    //     version_date: '2016-05-19',
-    //   })
-    //   visual_recognition.classify(params, function(err, response) {
-    //     if (err)
-    //     console.log('error', err);
-    //     else
-    //     console.log(JSON.stringify(response, null, 2))
-    //     var resultTemp= [];
-    //     let classesResponse = response.images[0].classifiers[0].classes;
-    //     classesResponse.forEach(el=>{
-    //       if(el.score > .60){
-    //         resultTemp.push(el)
-    //       }
-    //     })
-      //   let result = JSON.stringify(resultTemp, null, 2)
-      //   res.send(result)
-      // })
+     var params = {
+        images_file: fs.createReadStream(file.path),
+        'classifier_ids':[`${endpoint}`]
+      }
+      var visual_recognition = watson.visual_recognition({
+        api_key: process.env.WATSON_API,
+        version: 'v3',
+        version_date: '2016-05-19',
+      })
+      visual_recognition.classify(params, function(err, response) {
+        if (err)
+        console.log('error', err);
+        else
+        console.log(JSON.stringify(response, null, 2))
+        var resultTemp= [];
+        let classesResponse = response.images[0].classifiers[0].classes;
+        classesResponse.forEach(el=>{
+          if(el.score > .60){
+            resultTemp.push(el)
+          }
+        })
+        let result = JSON.stringify(resultTemp, null, 2)
+        res.send(result)
+      })
     // }).catch((error) => {
     //  console.log('error 2', error);
   //  })//END OF CATCH STATEMENT OF RESIZE
