6717d85202b86288c9e7d43d1e4120517207b48b
jmrigali <jmrigali@ymail.com>
2017-09-03 11:32:07 -0600

    adding compression

--- routes/watson.js
+++ routes/watson.js
@@ -19,36 +19,23 @@ const storage = multer.diskStorage({
 });
 const upload = multer({storage});
 
-const storageSmall = multer.diskStorage({
-  destination: './filesSmall',
-  filename(req, file, cb){
-    cb(null, 'temp.jpg');
-  }
-});
-const uploadSmall = multer({storageSmall});
+// const storageSmall = multer.diskStorage({
+//   destination: './filesSmall',
+//   filename(req, file, cb){
+//     cb(null, 'temp.jpg');
+//   }
+// });
+// const uploadSmall = multer({storageSmall});
 
 router.post('/:endpoint', upload.single('file'), (req,res,next)=>{
   let file = req.file;
   let meta = req.body;
   let endpoint = req.params.endpoint;
-  cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_SECRET
-  });
-  //  See Configuration Options for more details and additional configuration methods.
 
-  cloudinary.uploader.upload(file.path, function(result) {
-    // uploadSmall.single('result.url')
-    console.log(result.public_id);
-  },{crop:'fit', width:200, quality:'auto'})
-  .then((cloudUrl)=>{
-    cloudinary.v2.uploader.upload(cloudUrl.public_id,
-    { public_id: "filesSmall/tempImg" },
-    function(error, result) {console.log(result); console.log('./filesSmall/tempImg') });
-  //   console.log(cloudinary.image(cloudUrl.url, {type:"fetch"}));
-
-    // })
+    const Resizer = new resizer.Resizer([file.path])
+    // // use the Resizer.resize() method as a simple promise
+    Resizer.resize().then((filePaths) => {
+      console.log('filepaths', filePaths);
     var params = {
       images_file:fs.createReadStream(['./filesSmall/tempImg.jpg']),
       'classifier_ids':[`${endpoint}`],
@@ -84,7 +71,24 @@ router.post('/:endpoint', upload.single('file'), (req,res,next)=>{
         }
       })
   })//END OF THEN
+  .catch(err=>{console.log('Error', err);})
 
+  // cloudinary.config({
+  //   cloud_name: process.env.CLOUDINARY_NAME,
+  //   api_key: process.env.CLOUDINARY_API_KEY,
+  //   api_secret: process.env.CLOUDINARY_SECRET
+  // });
+  //  See Configuration Options for more details and additional configuration methods.
+
+  // cloudinary.uploader.upload(file.path, function(result) {
+    // uploadSmall.single('result.url')
+    // console.log(result.public_id);
+  // },{crop:'fit', width:200, quality:'auto'})
+  // .then((cloudUrl)=>{
+    // cloudinary.v2.uploader.upload(cloudUrl.public_id,
+    // { public_id: "filesSmall/tempImg" },
+    // function
+    // (error, result) {console.log(result); console.log('./filesSmall/tempImg') });
 
 
 
@@ -92,9 +96,7 @@ router.post('/:endpoint', upload.single('file'), (req,res,next)=>{
   // fs.writeFile('userimg.jpg', base64Data, 'base64', (err, suc) => {
   //   const filePathsToResize = [imgpath]
     // it can be just one, but still has to be an array, you can give it all of your images, it will just spit out images that are already below the configured max size (2MB) without doing any processing, so no need to check on your end
-    // const Resizer = new resizer.Resizer([file.path])
-    // // use the Resizer.resize() method as a simple promise
-    // Resizer.resize().then((filePaths) => {
+
       // console.log('FILEPATH: ', filePaths);
       // console.log('FILEPATH AT 0', filePaths[0]);
       // console.log('TYPE OF FILEPATHS', typeof filePaths);
